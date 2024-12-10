57efc4f77ae9aa2b503627b26a10ad9479308db9
Pavel Tyukin <me@patyukin.ru>
2024-12-10 18:14:30 +0300

    WIP

--- internal/usecase/get_payment_schedule_usecase.go
+++ internal/usecase/get_payment_schedule_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"fmt"
+
 	"github.com/patyukin/mbs-credits/internal/db"
 	"github.com/patyukin/mbs-credits/internal/model"
 	desc "github.com/patyukin/mbs-pkg/pkg/proto/credit_v1"
@@ -12,14 +13,16 @@ func (u *UseCase) GetPaymentScheduleUseCase(ctx context.Context, in *desc.GetPay
 	var err error
 	var payments []model.PaymentSchedule
 
-	err = u.registry.ReadCommitted(ctx, func(ctx context.Context, repo *db.Repository) error {
-		payments, err = repo.SelectPaymentScheduleByUserIDAndCreditID(ctx, in.UserId, in.CreditId)
-		if err != nil {
-			return fmt.Errorf("failed repo.SelectPaymentScheduleByUserIDAndCreditID: %w", err)
-		}
+	err = u.registry.ReadCommitted(
+		ctx, func(ctx context.Context, repo *db.Repository) error {
+			payments, err = repo.SelectPaymentScheduleByUserIDAndCreditID(ctx, in.UserId, in.CreditId)
+			if err != nil {
+				return fmt.Errorf("failed repo.SelectPaymentScheduleByUserIDAndCreditID: %w", err)
+			}
 
-		return nil
-	})
+			return nil
+		},
+	)
 	if err != nil {
 		return nil, fmt.Errorf("failed u.registry.ReadCommitted: %w", err)
 	}
